c4c60bff4791b5fa98c2821b9d6427d3dbd36988
Christopher Guess <cguess@gmail.com>
2015-09-22 01:19:46 +0200

    modified to use joomla test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
 
   def index
+    http = Net::HTTP.new("joomla-docker-129154.nitrousapp.com")
+    request = Net::HTTP::Get.new("/index.php?option=com_push&format=json&view=articles")
+    @response = JSON.parse(http.request(request).body)
+
+    #For the moment all images are empty, so we'll just leave this here
+    @response['results'].each do |result|
+      result['image_urls'] = []
+    end
+    
     respond_to do |format|
       format.json
     end
