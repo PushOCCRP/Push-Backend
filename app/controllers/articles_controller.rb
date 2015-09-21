95588c8624e614787e24cdd332cb21b12fa5ccc8
Christopher Guess <cguess@gmail.com>
2015-09-21 23:38:57 +0000

    working on a 500 error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,20 @@
 class ArticlesController < ApplicationController
 
   def index
-    http = Net::HTTP.new("joomla-docker-129154.nitrousapp.com")
-    request = Net::HTTP::Get.new("/index.php?option=com_push&format=json&view=articles")
-    @response = JSON.parse(http.request(request).body)
+    #http = Net::HTTP.new("joomla-docker-129154.nitrousapp.com")
+    #request = Net::HTTP::Get.new("/index.php?option=com_push&format=json&view=articles")
+    uri = URI.parse("http://joomla-docker-129154.nitrousapp.com/index.php?option=com_push&format=json&view=articles")
+
+    # Shortcut
+    @response = JSON.parse(Net::HTTP.get_response(uri).body)
+
+    #@response = JSON.parse(http.request(request).body)
 
     #For the moment all images are empty, so we'll just leave this here
     @response['results'].each do |result|
       result['image_urls'] = []
     end
-    
+
     respond_to do |format|
       format.json
     end
