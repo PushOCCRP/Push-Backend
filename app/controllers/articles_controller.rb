70c23f76617b64249c5685604ad0c4aad226496e
Christopher Guess <cguess@gmail.com>
2015-09-22 03:09:27 +0200

    testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,11 @@ class ArticlesController < ApplicationController
     uri = URI.parse("http://joomla-docker-129154.nitrousapp.com/index.php?option=com_push&format=json&view=articles")
 
     # Shortcut
-    @response_full = Net::HTTP.get_response(uri)
-    @response = JSON.parse(@response_full.body)
-    Rails.logger.info(@response_full)
+    #@response_full = Net::HTTP.get_response(uri)
+    #@response = JSON.parse(@response_full.body)
+    #Rails.logger.info(@response_full)
     #@response = JSON.parse(http.request(request).body)
-
+    @response = JSON.parse '{"results": [{"hello":""}, {"test":""}]}'
     #For the moment all images are empty, so we'll just leave this here
     @response['results'].each do |result|
       result['image_urls'] = []
