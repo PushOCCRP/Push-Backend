9e3ebff6a8c9669ff7c392c36b53bd0c300d950b
Christopher Guess <cguess@gmail.com>
2015-09-22 03:12:36 +0200

    testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,8 @@ class ArticlesController < ApplicationController
   def index
     #http = Net::HTTP.new("joomla-docker-129154.nitrousapp.com")
     #request = Net::HTTP::Get.new("/index.php?option=com_push&format=json&view=articles")
-    uri = URI.parse("http://joomla-docker-129154.nitrousapp.com/index.php?option=com_push&format=json&view=articles")
-
+    #uri = URI.parse("http://joomla-docker-129154.nitrousapp.com/index.php?option=com_push&format=json&view=articles")
+    uri = URI.pars("http://www.google.com")
     # Shortcut
     @response_full = Net::HTTP.get_response(uri)
     #@response = JSON.parse(@response_full.body)
