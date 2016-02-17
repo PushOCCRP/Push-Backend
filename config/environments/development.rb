1c0554dbd1ffa742a389c0d7ea6707001c2cedce
Christopher Guess <cguess@gmail.com>
2016-02-17 22:17:35 +0100

    added caching for server calls in newscoop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -39,4 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   # config.force_ssl = true
+  config.cache_store = :memory_store
+  
+
 end
