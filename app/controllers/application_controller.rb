bb772dc4805ef9807562ef7a021ea04a6fe55f2f
Christopher Guess <cguess@gmail.com>
2016-02-18 12:46:07 +0100

    added caching for joomla

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,6 @@ class ApplicationController < ActionController::Base
       end
       
       send_data image_response[:body], type: image_response[:content_type], disposition: 'inline'
-#      render text: image_response[:body], content_type: image_response[:headers]['content-type']
     end
     
   end
