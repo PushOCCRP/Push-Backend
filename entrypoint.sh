416212b348dd74ef36f3023a77ef1a378c0822ff
Christopher Guess <cguess@gmail.com>
2016-08-02 17:45:47 +0100

    fixed some bash stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,18 @@ bundle install --path vendor/bundle
 
 # make sure the last run is cleared
 rm /push/tmp/pids/server.pid
-echo "bundle done"
 
-if [ ! -z RAILS_ENV] && [ RAILS_ENV = "production" ]
+if [[ $RAILS_ENV = "development" ]]
 then
-	echo "Precompiling assets..."
-	rake assets:precompile
-	echo "Done."
-else
 	RAILS_ENV=development
 	echo "Skipping precompile in development"
+else
+        RAILS_ENV=production
+	echo "Precompiling assets..."
+	rake assets:precompile 
+	echo "Done."
 fi
+
 #if psql -lqt | cut -d \| -f 1 | grep -qw 'development'; then
 #    echo "database already exists"
 #else
