8d224cb98cec93d29a1f47003cbfc8a882f98430
Christopher Guess <cguess@gmail.com>
2016-08-22 16:36:17 +0200

    fixed more encoding issues for cyrillic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-
-	def sanitized_query query_string
-	    sanitized_query = query_string.unpack("U*").map{|c| c<256 ? c.chr : nil}.join
-	    return sanitized_query
-	end
 end
