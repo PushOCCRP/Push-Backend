3d292b3b703d032686b9948d1957d358594e3ad0
Christopher Guess <cguess@gmail.com>
2016-08-22 15:50:06 +0200

    added utf-8 to ascii conversion for making requests during searches

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def sanitized_query query_string
+	    sanitized_query = query_string.unpack("U*").map{|c| c<256 ? c.chr : nil}.join
+	    return sanitized_query
+	end
 end
