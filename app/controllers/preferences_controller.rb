8885a48332d50fe5087c1d9364b3a1458fb7464c
Ubuntu <ubuntu@ip-172-30-0-18.eu-west-1.compute.internal>
2016-11-11 11:23:18 +0000

    fixed title for categories saving

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -18,16 +18,22 @@ class PreferencesController < ApplicationController
 		else
 			@selected_categories = {}
 		end
+		
+		if(Setting.category_names != nil)
+  		@category_names = Setting.category_names.split('::')
+ 		else
+ 		  @selected_categories = {}
+ 		end
 	end
 
 	def update
-		
-		if(params[:category].count != params[:category_name])
-  		flash[:alert] = "Each category must have a display name"
-  		redirect_to :back
-  		return
-    end
-		
+		#if there is no seperate name set for a category it'll just set the name to be the default
+		index = 0
+		params[:category].each do |category|
+  		params[:category_name][index] = category if !category.blank? && params[:category_name][index].blank?
+      index += 1
+    end		
+    
 		# We do it this way so that there's no extra blanks hanging out
 		categories = ""
 		params[:category].each do |category|
@@ -39,7 +45,7 @@ class PreferencesController < ApplicationController
 		
  		category_names = ""
 		params[:category_name].each do |category_name|
-  		if(category_names != 0)
+  		if(category_names != 0 && category_names.length != 0)
     		category_names += '::'
       end
       category_names += category_name
@@ -72,21 +78,4 @@ class PreferencesController < ApplicationController
       return response
 	end
 	
-  def get_categories
-	  case @cms_mode 
-      when :occrp_joomla
-		# Not implemented yet
-	  when :wordpress
-        response = Wordpress.categories
-        response << 'post'
-        logger.debug(response)
-      when :newscoop
-		# Not implemented yet
-      when :cins_codeigniter
-		# Not implemented yet
-      end
-
-      return response
-	end
-
 end
