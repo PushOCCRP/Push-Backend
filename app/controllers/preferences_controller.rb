29e3f607b4dd6b161dca3924dffbc918a8fc5b93
Ubuntu <ubuntu@ip-172-30-2-7.eu-west-1.compute.internal>
2016-12-05 21:14:30 +0000

    fixed some yaml preferences stuff... again

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -12,58 +12,55 @@ class PreferencesController < ApplicationController
 		@categories = get_categories
     @category_names = Setting.category_names
 		@consolidated = Setting.consolidated_categories
+		@show_most_recent = Setting.show_most_recent_articles
 
-<<<<<<< cb6de339953f016818c57b1b493205c561549872
     @category_names = [] if @category_names.nil?
     
 		if(Setting.categories.nil? == false)
 			@selected_categories = YAML.load(Setting.categories)
 			@selected_categories = {} if @selected_categories.class == false
-=======
-		if(Setting.categories != nil)
-			@selected_categories = Setting.categories.split('::')
->>>>>>> working on preferences for newscoop
 		else
 			@selected_categories = {}
 		end
-		
+
+    CMS.languages().each{|language| @selected_categories[language] = [] if @selected_categories[language].blank?}
+
+
 		if(Setting.category_names != nil)
-  		@category_names = Setting.category_names.split('::')
- 		else
- 		  @category_names = {}
- 		end
+
+	    @category_names = YAML.load(Setting.category_names)
+    else
+      @category_names = {}
+    end
+    
+    CMS.languages().each{|language| @category_names[language] = [] if @category_names[language].blank?}
 	end
 
 	def update
-		#if there is no seperate name set for a category it'll just set the name to be the default
-		index = 0
-		params[:category].each do |category|
-  		params[:category_name][index] = category if !category.blank? && params[:category_name][index].blank?
-      index += 1
-    end		
-    
 		# We do it this way so that there's no extra blanks hanging out
-		categories = ""
-		
-		params[:category].each do |category|
-			if(categories.length != 0)
-				categories += '::'
-			end
-			categories += category
-		end
-		
- 		category_names = ""
-		params[:category_name].each do |category_name|
-  		if(category_names != 0 && category_names.length != 0)
-    		category_names += '::'
-      end
-      category_names += category_name
- 		end
+		categories = ""    
+    
+    params[:category].keys.each do |language|
+      params[:category][language].delete_if{|category| category.blank?}
+    end
 
-		Setting.categories = categories
-		Setting.category_names = category_names
+		params[:category].keys.each do |language|
+ 			index = 0	
+  		params[:category][language].each do |category|  			
+  			# if there's no category name for this one, add it the params
+  			params[:category_name][language][index] = category if params[:category_name][language][index].blank?
+  			index += 1
+  		end
+    end
+
+		Setting.categories = params[:category].to_yaml
+		Setting.category_names = params[:category_name].to_yaml
+		
 		Setting.consolidated_categories = params[:consolidated]
-
+    Setting.show_most_recent_articles = params[:show_most_recent]
+    
+    Rails.cache.clear
+    
 		flash[:notice] = "Categories successfully updated"
 		redirect_to action: :index
 	end
@@ -72,11 +69,11 @@ class PreferencesController < ApplicationController
 
 	def get_categories
 	  case @cms_mode 
-      when :occrp_joomla
+    when :occrp_joomla
 		# Not implemented yet
 	  when :wordpress
       response = Wordpress.categories
-      response << 'post'
+      logger.debug(response)
     when :newscoop
       response = Newscoop.categories
     when :cins_codeigniter
