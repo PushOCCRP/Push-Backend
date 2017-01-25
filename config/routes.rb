0d91baaf022856706e7e7bb372de950bbf45cd26
Ubuntu <ubuntu@ip-172-30-2-7.eu-west-1.compute.internal>
2017-01-25 19:16:09 +0000

    added heartbeat

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   put 'preferences' => 'preferences#update'
   
   get 'passthrough' => 'application#passthrough'
+  
+  get 'heartbeat' => 'application#heartbeat'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
