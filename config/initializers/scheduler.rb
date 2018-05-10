93f3b7eb314da9b81e4f7c365fdcd01dca412fae
root <root@ip-10-0-0-58.ec2.internal>
2018-05-10 21:20:56 +0000

    fixed categories caching

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -15,30 +15,39 @@ def run_update
   controller = ArticlesController.new
   cms_mode = ApplicationController.new.check_for_valid_cms_mode
   puts cms_mode
-  case cms_mode 
-    when :occrp_joomla
-      response = controller.get_occrp_joomla_articles(nil)
-    when :wordpress
-      response = Wordpress.articles(nil)
-    when :newscoop
-      response = Newscoop.articles(nil)
-    when :cins_codeigniter
-      response = CinsCodeigniter.articles(nil)
-    when :blox
-      response = Blox.articles(nil)
+  
+  # We run through a bunch of possibilities here making sure we cache everything we can.
+  booleans = ["true", "false"]
+  booleans.each do |boolean|
+    languages = CMS.languages
+    languages.each do|language|
+      case cms_mode 
+        when :occrp_joomla
+          response = controller.get_occrp_joomla_articles({'language' => language, 'categories' => boolean})
+        when :wordpress
+          response = Wordpress.articles({'language' => language, 'categories' => boolean})
+        when :newscoop
+          response = Newscoop.articles({'language' => language, 'categories' => boolean})
+        when :cins_codeigniter
+          response = CinsCodeigniter.articles({'language' => language, 'categories' => boolean})
+        when :blox
+          response = Blox.articles({'language' => language, 'categories' => boolean})
+      end
+      
+      application_controller = ApplicationController.new
+      
+      response[:results].keys.each do |key|
+        #go through each image
+        response[:results][key].each do |article|
+          article['images'].each{|image| application_controller.passthrough_image(image['url'])}
+        end unless response[:results][key].nil?
+      end if response[:results].class == Hash
+      
+      response[:results].each do |article|
+        article['images'].each{|image| application_controller.passthrough_image(image['url'])}
+      end if response[:results].class == Array
+    end
   end
-  
-  application_controller = ApplicationController.new
-  response[:results].keys.each do |key|
-    #go through each image
-    response[key].each do |article|
-      article['images'].each{|image| application_controller.passthrough_image(image['url'])}
-    end unless response[key].nil?
-  end if response[:results].class == Hash
-  
-  response[:results].each do |article|
-    article['images'].each{|image| application_controller.passthrough_image(image['url'])}
-  end if response[:results].class == Array
   
 end
 
