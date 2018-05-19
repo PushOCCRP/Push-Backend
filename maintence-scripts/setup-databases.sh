3da0059a2b85526a0200033e3e22bee7650c158b
Christopher Guess <cguess@gmail.com>
2018-05-19 16:44:07 -0500

    fixing the setup (again)

---
+++ maintence-scripts/setup-databases.sh
@@ -0,0 +1,18 @@
+# Stop any possible docker-compose containers that might be sticking around
+kill_docker_containers
+
+# Migrate the database (does nothing if database already exists)
+echoc "\n-------------------------------------------------------------------------------------------------------------\n" $LIGHT_BLUE
+echoc "Migrating the Push app database" $LIGHT_BLUE
+echoc "\n-------------------------------------------------------------------------------------------------------------\n\n" $LIGHT_BLUE
+
+docker-compose run web rake db:create
+docker-compose run web rake db:migrate
+docker-compose run web rake db:seed
+
+echoc "\n-------------------------------------------------------------------------------------------------------------\n" $LIGHT_BLUE
+echoc "Everything should be set up properly now."
+echoc "Try running"
+echoc "docker-compose up" $YELLOW
+echoc "to make sure it works"
+echoc "\n-------------------------------------------------------------------------------------------------------------\n\n" $LIGHT_BLUE
